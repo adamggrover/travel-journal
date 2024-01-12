e30b9291eebac7b40ce39d2372279b9e1a7bea18
adamggrover <93373074+adamggrover@users.noreply.github.com>
2024-01-12 13:03:58 +0000

    customise trips

--- src/data.js
+++ src/data.js
@@ -1,34 +1,149 @@
 export default [
     {
         id: 1,
-        title: "Mount Fuji",
-        location: "JAPAN",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Dartmoor",
+        location: "UK",
+        googleMapsUrl: "https://www.google.com/maps/place/Dartmoor+National+Park/@50.5718891,-3.9232633,17z/data=!3m1!4b1!4m6!3m5!1s0x486d00cf28cd19ff:0xb6f41325fa1a8bc1!8m2!3d50.5718857!4d-3.9206884!16zL20vMDI4bm0?entry=ttu",
+        startDate: "23 Oct, 2023",
+        endDate: "25 Oct, 2023",
+        description: "Dartmoor is an upland area in southern Devon, South West England. The moorland and surrounding land has been protected by National Park status since 1951.",
+        imageUrl: "https://images.unsplash.com/photo-1606042317688-a630159005e9?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
 
     {
         id: 2,
-        title: "Sydney Opera House",
-        location: "AUSTRALIA",
-        googleMapsUrl: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567799,151.2127218,17z/data=!3m1!4b1!4m6!3m5!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0?entry=ttu",
-        startDate: "27 May, 2021",
-        endDate: "8 Jun, 2021",
-        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
-        imageUrl: "https://images.unsplash.com/photo-1590082037560-93eb9eaed7ab?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        title: "Omodos",
+        location: "CYPRUS",
+        googleMapsUrl: "https://www.google.com/maps/place/Omodos+4760,+Cyprus/@34.8485315,32.8037039,16z/data=!3m1!4b1!4m6!3m5!1s0x14e738b685884307:0xfec187edc0776f13!8m2!3d34.8489219!4d32.8077875!16zL20vMGNkZndq?entry=ttu",
+        startDate: "21 Aug, 2023",
+        endDate: "24 Aug, 2023",
+        description: "Omodos is a village in the Troödos Mountains of Cyprus. It is also located in the Limassol District of Cyprus and is 80 kilometers from the city of Nicosia. ",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/3/34/OmodosMar152023_02.jpg"
     },
 
     {
         id: 3,
-        title: "Geirangerfjord",
-        location: "NORWAY",
-        googleMapsUrl: "https://www.google.com/maps/place/Geirangerfjord/@62.1046586,6.7628447,11z/data=!3m1!4b1!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi?entry=ttu",
-        startDate: "01 Oct, 2021",
-        endDate: "18 Nov, 2021",
-        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        imageUrl: "https://images.unsplash.com/photo-1663150848604-74b3bd874484?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8R2VpcmFuZ2VyZmpvcmR8ZW58MHwxfDB8fHww"
-    }
+        title: "Tavira",
+        location: "Portugal",
+        googleMapsUrl: "https://www.google.com/maps/place/Tavira,+Portugal/@37.129471,-7.6762365,13z/data=!3m1!4b1!4m6!3m5!1s0xd10037cd08e6625:0xd44503c5392ddd45!8m2!3d37.1335906!4d-7.6430019!16s%2Fm%2F0fpglk3?entry=ttu",
+        startDate: "10 May, 2019",
+        endDate: "18 May, 2019",
+        description: "Tavira is a Portuguese town and municipality, capital of the Costa do Acantilado, situated in the east of the Algarve on the south coast of Portugal.",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Cidade_de_Tavira_edited.jpg"
+    },
+
+    {
+        id: 4,
+        title: "Rome",
+        location: "Italy",
+        googleMapsUrl: "https://www.google.com/maps/place/Metropolitan+City+of+Rome+Capital,+Italy/@41.8509436,11.8555119,9z/data=!3m1!4b1!4m6!3m5!1s0x13258a111bd74ac3:0x3094f9ab2388100!8m2!3d41.9027008!4d12.4962352!16s%2Fm%2F012m5qrv?entry=ttu",
+        startDate: "10 Oct, 2017",
+        endDate: "14 Oct, 2017",
+        description: "Rome is the capital city of Italy. It is also the capital of the Lazio region, the centre of the Metropolitan City of Rome Capital, and a special comune (municipality) named Comune di Roma Capitale",
+        imageUrl: "https://images.unsplash.com/photo-1531572753322-ad063cecc140?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 5,
+        title: "Granada",
+        location: "SPAIN",
+        googleMapsUrl: "https://www.google.com/maps/place/Granada,+Spain/@37.1810075,-3.6324714,13z/data=!3m1!4b1!4m6!3m5!1s0xd71fce62d32c27d:0x9258f79dd3600d72!8m2!3d37.1774605!4d-3.5984368!16zL20vMDlqcTA?entry=ttu",
+        startDate: "18 Aug, 2017",
+        endDate: "28 Aug, 2017",
+        description: "Granada is the capital city of the province of Granada, in the autonomous community of Andalusia, Spain.",
+        imageUrl: "https://plus.unsplash.com/premium_photo-1702598408804-2aee4ab103af?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YWxoYW1icmElMjBncmFuYWRhfGVufDB8fDB8fHww"
+    },
+
+    {
+        id: 6,
+        title: "Florence",
+        location: "ITALY",
+        googleMapsUrl: "https://www.google.com/maps/place/Metropolitan+City+of+Florence,+Italy/@43.8448538,10.5726501,9z/data=!3m1!4b1!4m6!3m5!1s0x132af8a078bd7663:0x3082c90e3e48060!8m2!3d43.7679178!4d11.2523792!16s%2Fg%2F11b6cmkpxv?entry=ttu",
+        startDate: "27 May, 2016",
+        endDate: "28 May, 2016",
+        description: "Florence is the capital city of the Italian region of Tuscany. It is also the most populated city in Tuscany.",
+        imageUrl: "https://images.unsplash.com/photo-1543429258-cc721a300e8a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 7,
+        title: "Amsterdam",
+        location: "NETHERLANDS",
+        googleMapsUrl: "https://www.google.com/maps/place/Amsterdam,+Netherlands/@52.3547607,4.739154,11z/data=!3m1!4b1!4m6!3m5!1s0x47c63fb5949a7755:0x6600fd4cb7c0af8d!8m2!3d52.3675734!4d4.9041389!16zL20vMGszcA?entry=ttu",
+        startDate: "21 Feb, 2016",
+        endDate: "24 Feb, 2016",
+        description: "Ansterdam is the capital and most populated city of the Netherlands,",
+        imageUrl: "https://images.unsplash.com/photo-1584003564911-a7a321c84e1c?q=80&w=1968&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 8,
+        title: "Ko Lanta",
+        location: "THAILAND",
+        googleMapsUrl: "https://www.google.com/maps/place/Ko+Lanta/@7.6340907,99.0199917,15z/data=!3m1!4b1!4m6!3m5!1s0x304e033c84ca4bad:0x9a53bffbdc797fb3!8m2!3d7.6340697!4d99.0302915!16s%2Fg%2F11fls3t6rm?entry=ttu",
+        startDate: "06 Jan, 2017",
+        endDate: "29 Jan, 2017",
+        description: "Ko Lanta Yai is an island in the Strait of Malacca off the west coast of Thailand, between the Phi Phi Islands and the mainland",
+        imageUrl: "https://images.unsplash.com/photo-1691246983119-99b34374df7e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 9,
+        title: "Tokyo",
+        location: "JAPAN",
+        googleMapsUrl: "https://www.google.com/maps/place/Tokyo,+Japan/@35.5042958,138.4504876,8z/data=!3m1!4b1!4m6!3m5!1s0x605d1b87f02e57e7:0x2e01618b22571b89!8m2!3d35.6764225!4d139.650027!16zL20vMDdkZms?entry=ttu",
+        startDate: "29 Dec, 2016",
+        endDate: "06 Jan, 2017",
+        description: "Tokyo, officially the Tokyo Metropolis, is the capital of Japan and the most populous city in the world ",
+        imageUrl: "https://images.unsplash.com/photo-1503899036084-c55cdd92da26?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 10,
+        title: "Isle of Skye",
+        location: "UK",
+        googleMapsUrl: "https://www.google.com/maps/place/Skye/@57.3619878,-6.8773588,9z/data=!3m1!4b1!4m6!3m5!1s0x488c312261bdc1ff:0x585c356fe9c86218!8m2!3d57.2736278!4d-6.2155023!16zL20vMDN5Nms?entry=ttu",
+        startDate: "28 Mar, 2013",
+        endDate: "15 Sep, 2013",
+        description: "The Isle of Skye, or simply Skye, is the largest and northernmost of the major islands in the Inner Hebrides of Scotland",
+        imageUrl: "https://images.unsplash.com/photo-1583243151505-33e6ff0ffe07?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 11,
+        title: "Hampi",
+        location: "India",
+        googleMapsUrl: "https://www.google.com/maps/place/Hampi,+Karnataka+583239,+India/@15.3332001,76.4581848,16z/data=!3m1!4b1!4m6!3m5!1s0x3bb77fd95d4be823:0x6e52e05076df36b8!8m2!3d15.3350132!4d76.460024!16s%2Fg%2F11bc636x1n?entry=ttu",
+        startDate: "30 Nov, 2012",
+        endDate: "1 Mar, 2013",
+        description: "Hampi or Hampe, also referred to as the Group of Monuments at Hampi, is a UNESCO World Heritage Site located in Hampi (City), Ballari district now Vijayanagara district, east-central Karnataka, India.",
+        imageUrl: "https://images.unsplash.com/photo-1620766182966-c6eb5ed2b788?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 12,
+        title: "Berlin",
+        location: "Germany",
+        googleMapsUrl: "https://www.google.com/maps/place/Berlin,+Germany/@52.5068042,13.095097,10z/data=!3m1!4b1!4m6!3m5!1s0x47a84e373f035901:0x42120465b5e3b70!8m2!3d52.5200066!4d13.404954!16zL20vMDE1NnE?entry=ttu",
+        startDate: "20 Oct, 2011",
+        endDate: "24 Oct, 2011",
+        description: "Berlin is the capital and largest city of Germany by both area and population.It has more than 3.85 million inhabitants making it the European Union's most populous city",
+        imageUrl: "https://images.unsplash.com/photo-1496968594384-1736c036e4e6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 13,
+        title: "Marrakech",
+        location: "MORROCCO",
+        googleMapsUrl: "https://www.google.com/maps/place/Marrakesh,+Morocco/@31.6347412,-8.0902545,12z/data=!3m1!4b1!4m6!3m5!1s0xdafee8d96179e51:0x5950b6534f87adb8!8m2!3d31.6225224!4d-7.9898258!16zL20vMDU0cnc?entry=ttu",
+        startDate: "10 Sep, 2010",
+        endDate: "24 Sep, 2010",
+        description: "Marrakech is the fourth largest city in Morocco. It is one of the four imperial cities of Morocco and the capital of the Marrakesh–Safi region",
+        imageUrl: "https://images.unsplash.com/photo-1548018560-4cb48a8837c1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+
+
+
+
 ]
